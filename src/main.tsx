68dec73de66b19abec3b1ce2627131ce38de7c75
Marcos Delay <dev.marcosdly@gmail.com>
2024-04-12 06:12:16 -0300

    feat: common style

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { render } from "preact";
 import { App } from "./App";
 import { setUITheme, Theme } from "./lib/theme";
+import "./main.scss";
 
 render(<App />, document.getElementById("app")!);
 
